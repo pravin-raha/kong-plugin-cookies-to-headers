43b4df2491f4d14f5dce62f516c5e852c4203ce7
Pravin Rahangdale <pravinrahangdale41@gmail.com>
2022-10-18 20:50:55 +0530

    Update schema.lua

--- schema.lua
+++ schema.lua
@@ -1,7 +1,7 @@
 return {
   no_consumer = true,
   fields = {
-   cookie_name = { type = "string", default="SESSION" },
-   header_name = { type = "string", default="Authorization" }
+   cookie_name = { type = "string", default="SESSION", required = false },
+   header_name = { type = "string", default="Authorization", required = false }
   }
 }

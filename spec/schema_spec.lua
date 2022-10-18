34b36f4546ebf393a04243120e2f9d6b4cf766dc
Pravin Rahangdale <pravinrahangdale41@gmail.com>
2022-10-18 23:31:43 +0530

    Fix test

--- spec/schema_spec.lua
+++ spec/schema_spec.lua
@@ -1,12 +1,26 @@
-local schema = require('../schema')
+local PLUGIN_NAME = "cookies-to-headers"
 
-describe("TestSchema", function()
-    it("should initialize schema correctly", function()
-        assert.is_true(schema.no_consumer)
-        assert.equal("string", schema.fields.cookie_name.type)
-        assert.equal(false, schema.fields.cookie_name.required)
-        assert.equal("SESSION", schema.fields.cookie_name.default)
-        assert.equal("Authorization", schema.fields.header_name.default)
+
+-- helper function to validate data against a schema
+local validate
+do
+    local validate_entity = require("spec.helpers").validate_plugin_config_schema
+    local plugin_schema = require("kong.plugins." .. PLUGIN_NAME .. ".schema")
+
+    function validate(data)
+        return validate_entity(data, plugin_schema)
+    end
+end
+
+describe(PLUGIN_NAME .. ": (schema)", function()
+
+    it("should initialize schema correct", function()
+        local ok, err = validate({
+            cookie_name = "SESSION",
+            header_name = "Authorization",
+        })
+        assert.is_nil(err)
+        assert.is_truthy(ok)
     end)
 
 end)
